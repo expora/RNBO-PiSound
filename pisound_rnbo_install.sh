17baf734d3d2ce4f6ca40808b129be528495da7d
expora <expora@gmail.com>
2024-08-11 21:58:12 -0600

    Add config backup step to installation script

--- pisound_rnbo_install.sh
+++ pisound_rnbo_install.sh
@@ -11,7 +11,8 @@ chmod +x ./rnbo_btn_scripts/*.sh
 # copy the conf file into the correct place
 cp ./rnbo_btn_scripts/*.sh /usr/local/pisound/scripts/pisound-btn/
 
-# copy the conf file into the correct place
+# copy the conf file into the correct place, backing up the original one
+cp /etc/pisound.conf /etc/pisound.conf.orig
 cp ./rnbo_btn_scripts/pisound.conf /etc/pisound.conf
 
 echo "Done! Continue the tutorial to test and run the examples. Happy patching :)"
